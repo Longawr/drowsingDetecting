e30355f3b7174679a61b6b88b3940e6a0e52cbcc
Longawr <aliezlong@gmail.com>
2023-02-19 22:23:08 +0700

    change cal_EAR function, add cal_MAR function

--- Readme.md
+++ Readme.md
@@ -1,9 +1,10 @@
 # drowsinessDetector
 OpenCV Drowsiness Detector using Python and Dlib
+using: python 3.10
 requires:
     'opencv-contrib-python  4.7.0.68'
+    '               cmake   any'
     '               Dlib    19.22.99'
-    '               python  3.10'
     '               scipy   1.10.0'
     '               numpy   1.24.2'
     '               pygame  2.1.3'
--- drowsinessDetector.py
+++ drowsinessDetector.py
@@ -11,23 +11,37 @@ warningSound = mixer.Sound("Scream.wav")
 
 #status marking for current state
 sleep = 0
-drowsy = 0
+# drowsy = 0
 active = 0
 status=""
 color=(0,0,0)
 warning = False
+
+# def calculate_EAR(eye):
+# 	A = distance.euclidean(eye[1], eye[5])
+# 	B = distance.euclidean(eye[2], eye[4])
+# 	C = distance.euclidean(eye[0], eye[3])
+# 	EAR = (A+B)/(2.0*C)
+# 	if(EAR>0.22):
+# 		return 2
+# 	elif(EAR>0.18 and EAR<=0.22):
+# 		return 1
+# 	else:
+# 		return 0
 
 def calculate_EAR(eye):
 	A = distance.euclidean(eye[1], eye[5])
 	B = distance.euclidean(eye[2], eye[4])
 	C = distance.euclidean(eye[0], eye[3])
 	EAR = (A+B)/(2.0*C)
-	if(EAR>0.22):
-		return 2
-	elif(EAR>0.18 and EAR<=0.22):
-		return 1
-	else:
-		return 0
+	return EAR
+
+def calculate_MAR(mount): # 49 55 52 58
+	A = distance.euclidean(mount[0], mount[9])
+	B = distance.euclidean(mount[6], mount[9])
+	C = distance.euclidean(mount[3], mount[9])
+	return ((A + B) / C)
+
 
 while True:
 	_, frame = cap.read()
@@ -39,6 +53,7 @@ while True:
 		face_landmarks = dlib_facelandmark(gray, face)
 		leftEye = []
 		rightEye = []
+		mount = []
 
 		for n in range(36,42):
 			x = face_landmarks.part(n).x
@@ -62,29 +77,41 @@ while True:
 			y2 = face_landmarks.part(next_point).y
 			cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
 		
+		for n in range(49, 61):
+			x = face_landmarks.part(n).x
+			y = face_landmarks.part(n).y
+			cv2.circle(frame,(x,y),0,(0,255,0),-1)
+			mount.append((x,y))
+		
 		leftEar = calculate_EAR(leftEye)
 		rightEar = calculate_EAR(rightEye)
+		earBlink = (leftEar + rightEar)/2
 
-		if(leftEar==0 and rightEar==0):
+		MAR = calculate_MAR(mount)
+
+		if(earBlink < 0.2 and MAR < 1.8):
 			sleep+=1
-			drowsy=0
+			# drowsy=0
 			active=0
-			if(sleep>20):
+			if(sleep>30):
 				warning = True
 				status="DIEEEE !!!"
 				color = (255,0,0)
+			elif (sleep>10):
+				status="Blink detected !"
+				color = (255,0,0)
 
-		elif(leftEar==1 or rightEar==1):
-			sleep=0
-			active=0
-			drowsy+=1
-			if(drowsy>6):
-				warning = False
-				status="You'll die !"
-				color = (0,0,255)
+		# elif(leftEar==1 or rightEar==1):
+		# 	sleep=0
+		# 	active=0
+		# 	drowsy+=1
+		# 	if(drowsy>6):
+		# 		warning = False
+		# 		status="You'll die !"
+		# 		color = (0,0,255)
 
 		else:
-			drowsy=0
+			# drowsy=0
 			sleep=0
 			active+=1
 			if(active>6):
@@ -95,6 +122,8 @@ while True:
 		
 		warningSound.play(-1) if warning else warningSound.stop()
 		cv2.putText(frame, status, (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color,2)
+		cv2.putText(frame, f"EAR: {round(earBlink,2)}", (100,150), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color,2)
+		cv2.putText(frame, f"MAR: {round(MAR, 2)}", (50,200), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color,2)
 
 	cv2.imshow("Are you Sleepy", frame)
 
